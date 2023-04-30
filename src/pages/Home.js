e7bf36e01caa2fc4767902da3841926326e0aaef
Sahil singh <120320414+sahil053@users.noreply.github.com>
2023-04-30 15:21:08 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+import banner from "../assets/banner.svg"
+import features from "../assets/features.svg"
+
+function Home(){
+    return <div>
+        
+    </div>
+}
+
+export default Home
